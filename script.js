d18b8f02c718c91df05f6c9180fa613277439aab
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-22 01:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const getSum = () => {
 	let rows = document.querySelectorAll("table tr");
 	let sum =0;
 	for(let i=1; i<rows.length; i++){
-		let val = row[i].children[1];
+		let val = rows[i].children[1];
 		sum += Number(val.textContent);
 	}
     const newRow = document.createElement("tr");
