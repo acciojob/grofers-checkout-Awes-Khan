495efc00e9a93e1c52585ab38b05257d21a9531f
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-22 01:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
+	let rows = document.querySelectorAll("table tr");
+	let sum =0;
+	for(let i=1; i<rows.length; i++){
+		let val = row[i].children[1];
+		sum += Number(val.textContent);
+	}
 //Add your code here
+	return sum;
   
 };
 
